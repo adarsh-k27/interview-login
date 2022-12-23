7dfe5c62ddb417c3fbb0ef8620c14fb511e05bc4
adarsh-k27 <adarshkdev27@gmail.com>
2022-12-23 20:11:20 +0530

    added link

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const AxiosInstance=axios.create({
-    baseURL:"http://localhost:5000/api"
+    baseURL: "https://interview-backend.onrender.com//api"
 })
 
 export default AxiosInstance;
